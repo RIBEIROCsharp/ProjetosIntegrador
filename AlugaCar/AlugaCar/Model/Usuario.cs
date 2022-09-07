441d450144f9cf9956ca80e8a09be38e4a3543ca
felipeeC <felipe.moraes.alves@hotmail.com>
2022-09-07 18:18:13 -0300

    update user model class

--- AlugaCar/AlugaCar/Model/Usuario.cs
+++ AlugaCar/AlugaCar/Model/Usuario.cs
@@ -5,6 +5,10 @@
         public int cpf { get; set; }
         public string nome { get; set; }
         public string sobrenome { get; set; }
-        
+        public int idade { get; set; }
+        public string sexo { get; set; }
+        public int telefone { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
     }
 }
